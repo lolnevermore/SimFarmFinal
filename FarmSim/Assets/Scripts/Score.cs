f6284b932938b9831d7e6faace36aa9c84130d94
ChiefKheath <kh1469s@gre.ac.uk>
2023-05-05 18:25:52 +0100

    Score ammendments

--- FarmSim/Assets/Scripts/Score.cs
+++ FarmSim/Assets/Scripts/Score.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
@@ -27,6 +29,11 @@ public class Score : MonoBehaviour
         
 
         ScoreCheckAmounts();
+
+        /*if(timer.currentTime == 0)
+        {
+            score = 0;
+        }*/
     }
 
 
@@ -39,60 +46,100 @@ public class Score : MonoBehaviour
                 {
                     print("failure");
                 }
+                else if (score >= 50)
+                {
+                    print("success");
+                    Time.timeScale = 0f;
+                    
+                }
                 break;
 
             case 2:
-                if (score < 150)
+                if (score < 100)
                 {
                     print("failure");
+                }
+                else if (score >= 100)
+                {
+                    print("success");
+                    Time.timeScale = 0f;
                 }
                 break;
 
             case 3:
-                if (score < 300)
+                if (score < 200)
                 {
                     print("failure");
+                }
+                else if (score >= 200)
+                {
+                    print("success");
+                    Time.timeScale = 0f;
                 }
                 break;
 
             case 4:
-                if (score < 600)
+                if (score < 350)
                 {
                     print("failure");
+                }
+                else if (score >= 350)
+                {
+                    print("success");
+                    Time.timeScale = 0f;
                 }
 
                 break;
 
             case 5:
-                if (score < 1000)
+                if (score < 550)
                 {
                     print("failure");
+                }
+                else if (score >= 550)
+                {
+                    print("success");
+                    Time.timeScale = 0f;
                 }
 
                 break;
 
             case 6:
-                if(score < 1500)
+                if(score < 700)
                 {
                     print("failure");
+                }
+                else if (score >= 700)
+                {
+                    print("success");
+                    Time.timeScale = 0f;
                 }
                 break;
 
             case 7:
-                if(score < 2300)
+                if(score < 800)
                 {
                     print("failure");
+                }
+                else if (score >= 800)
+                {
+                    print("success");
+                    Time.timeScale = 0f;
                 }
                 break;
 
             case 8:
                 {
-                    if (score >= 3000)
-                    {
-                        print("Success");
-                    }
-                    break;
-
+                if (score < 1000)
+                {
+                    print("failure");
+                }
+                else if (score >= 1000)
+                {
+                    print("success");
+                    Time.timeScale = 0f;
+                }
+                break;
                 }
 
         }
