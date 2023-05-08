8f0d26ef7fcab301fad79da20f62dcb2c7a3ed2f
rr5510s <78934339+rrodney077@users.noreply.github.com>
2023-05-08 22:22:34 +0100

    Movement fix and crate fix

--- FarmSim/Assets/Scripts/Movement.cs
+++ FarmSim/Assets/Scripts/Movement.cs
@@ -76,20 +76,22 @@ public class Movement : MonoBehaviour
 
     void Move()
     {
-        //Vector3 direction = new Vector3(movementInput.x, 0, movementInput.y);
-        Vector3 rightMov = right * speed * Time.deltaTime * movementInput.x;
-        Vector3 upMov = forward * speed * Time.deltaTime * movementInput.y;
+        if (movementInput.x != 0 || movementInput.y != 0)
+        {
+           
+            Vector3 rightMov = right * speed * Time.deltaTime * movementInput.x;
+            Vector3 upMov = forward * speed * Time.deltaTime * movementInput.y;
 
-        Vector3 heading = Vector3.Normalize(rightMov + upMov);
+            Vector3 heading = Vector3.Normalize(rightMov + upMov);
 
-        transform.forward = heading;
+            transform.forward = heading;
 
-        //Vector3 movement = Vector3.Normalize(heading) * speed * Time.deltaTime;
 
-        transform.position += rightMov;
-        transform.position += upMov;
-        //transform.position += movement;        
-        //camSwitcher.GetComponent<CamSwitcher>().isMoveable = false;
+            transform.position += rightMov;
+            transform.position += upMov;
+          
+        }
+
 
         if (movementInput.x != 0 || movementInput.y != 0)
         {
