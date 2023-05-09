afe39d818ac7cad731556d5f9d51733fea9ea86c
ChiefKheath <kh1469s@gre.ac.uk>
2023-05-09 04:30:19 +0100

    Majority of UI functionality sorted

--- FarmSim/Assets/Scripts/PauseMenu.cs
+++ FarmSim/Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool IsPaused = false;
+    public GameObject PauseUI;
+
+    private void Start()
     {
-        
+        PauseUI = GameObject.Find("PauseUI");
+        PauseUI.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void QuitToMain()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
